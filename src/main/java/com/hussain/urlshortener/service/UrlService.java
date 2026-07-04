b55ad65d9427c635a582ee71743d08bea9d14c38
Yasser Hussain <yasserhussain1110@gmail.com>
2026-07-04 22:18:01 +0530

    Optimizing write path

--- src/main/java/com/hussain/urlshortener/service/UrlService.java
+++ src/main/java/com/hussain/urlshortener/service/UrlService.java
@@ -5,7 +5,6 @@ import com.hussain.urlshortener.exception.UrlNotFoundException;
 import com.hussain.urlshortener.model.Url;
 import com.hussain.urlshortener.repository.UrlRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -14,13 +13,16 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class UrlService {
     private final UrlRepository urlRepository;
 
     public Url shorten(Url url) {
-        return urlRepository.findByOriginalUrl(url.getOriginalUrl())
-                .orElseGet(() -> getOrSaveUrlOnDatabase(url));
+        try {
+            return urlRepository.save(url);
+        } catch (DataIntegrityViolationException e) {
+            return urlRepository.findByOriginalUrl(url.getOriginalUrl())
+                    .orElseThrow();
+        }
     }
 
     @Cacheable(value = CacheConfig.URLS_CACHE, key = "#id")
@@ -30,15 +32,5 @@ public class UrlService {
 
     public List<Url> findAll() {
         return urlRepository.findAll();
-    }
-
-    private Url getOrSaveUrlOnDatabase(Url url) {
-        try {
-            return urlRepository.save(url);
-        } catch (DataIntegrityViolationException e) {
-            log.warn("Tried to save pre-existing url: {}", url.getOriginalUrl(),  e);
-            return urlRepository.findByOriginalUrl(url.getOriginalUrl())
-                    .orElseThrow();
-        }
     }
 }
